ab8b6be87ae00ad632b24132ca18df131f5d57ee
zhangyannan <401119582@qq.com>
2017-02-15 18:00:04 +0800

    截屏

--- 屏幕截取/ViewController.m
+++ 屏幕截取/ViewController.m
@@ -17,6 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //开启图形上下文
+    UIGraphicsBeginImageContextWithOptions(self.view.frame.size, NO, 0);
+    //获取当前上下文
+    CGContextRef context=UIGraphicsGetCurrentContext();
+    //把控件上的图层渲染到当前的上下文
+    [self.view.layer renderInContext:context];
+    //获取当前的图片
+    UIImage *image=UIGraphicsGetImageFromCurrentImageContext();
+    //将当前图片转换为二进制数据
+    NSData *data=UIImagePNGRepresentation(image);
+    //写到桌面上
+    [data writeToFile:@"/Users/shanghaijunheng/Desktop/test.png" atomically:YES];
 }
 
 
